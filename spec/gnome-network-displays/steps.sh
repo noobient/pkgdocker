1e39b8c9fcdcd53ca11423d78f54eb3ccbd01df9
Viktor Berke <github.bviktor@outlook.com>
2021-09-08 15:21:11 +0200

    Add gnome-network-displays

---
+++ spec/gnome-network-displays/steps.sh
@@ -0,0 +1,23 @@
+sed -i.orig 's/^# deb-src /deb-src /' /etc/apt/sources.list
+apt update
+
+wget https://gitlab.gnome.org/GNOME/gnome-network-displays/-/archive/v${PKG_VER}/gnome-network-displays-v${PKG_VER}.tar.gz
+tar xf gnome-network-displays-v${PKG_VER}.tar.gz
+cd gnome-network-displays-v${PKG_VER}
+
+mkdir -p build && cd build
+meson .. --prefix=/usr
+ninja -j$(nproc)
+
+mkdir -p "${DEST_DIR}/etc/polkit-1/localauthority/20-org.d"
+cat << EOF > "${DEST_DIR}/etc/polkit-1/localauthority/20-org.d/99-firewalld-inspect.pkla"
+[FirewallD config inspection for users]
+Identity=unix-user:*
+Action=org.fedoraproject.FirewallD1.config.info
+ResultAny=auth_admin_keep
+ResultInactive=auth_admin_keep
+ResultActive=yes
+EOF
+chmod 0640 "${DEST_DIR}/etc/polkit-1/localauthority/20-org.d/99-firewalld-inspect.pkla"
+
+DESTDIR="${DEST_DIR}" ninja install
